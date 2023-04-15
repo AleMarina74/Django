793d6e9208d752db0c0dae99a34af37ccbc7b771
Emi <35235320+elbarril@users.noreply.github.com>
2023-04-15 20:08:33 -0300

    hardcoded heading in home fix

--- Biblioteca/Biblioteca/views.py
+++ Biblioteca/Biblioteca/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+ENCABEZADO = "Mesa de Entrada"
 # Create your views here.
 def home(request):
-    return render(request=request, template_name='base.html', context={})
+    return render(request=request, template_name='base.html', context={"encabezado":ENCABEZADO})
