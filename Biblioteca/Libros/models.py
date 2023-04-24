ee918f1158e57c875976ab47065c53949b8c431d
AleMarina74 <ahilenrocio@gmail.com>
2023-04-24 08:11:33 -0300

    Libros modelo listo

--- Biblioteca/Libros/models.py
+++ Biblioteca/Libros/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Libro(models.Model):
-    pass
+    USBN = models.CharField(max_length=18, null=True)
+    titulo = models.CharField(max_length=200,null=True)
+    autor = models.CharField(max_length=100,null=True)
+    estado = models.CharField(max_length=1, default='D')
+    dni = models.CharField(max_length=8, default = '', blank =True)
